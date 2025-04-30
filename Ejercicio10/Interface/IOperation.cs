8d564a14004895a5f0d0e2788c72455e1e97ed63
RanMd <randommid1873@gmail.com>
2025-04-30 16:56:16 -0500

    Modify folder structure

---
+++ Ejercicio10/Interface/IOperation.cs
@@ -0,0 +1,6 @@
+using Ejercicio10;
+
+public abstract class FractionOperation
+{
+  public abstract double Execute(double currentResult, string fraction, Converter converter);
+}
--- Ejercicio10/Model/Operation.cs
+++ Ejercicio10/Model/Operation.cs
@@ -1,10 +1,5 @@
 using Ejercicio10;
 
-public abstract class FractionOperation
-{
-  public abstract double Execute(double currentResult, string fraction, Converter converter);
-}
-
 public class AdditionOperation : FractionOperation
 {
   public override double Execute(double currentResult, string fraction, Converter converter)
